20d3458c35b0c565b278e6f2280e2febc0b3e779
Tetiana2023 <drofaj18@gmail.com>
2023-03-23 22:32:22 +0200

    last done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,67 +6,70 @@ import { FilterContact } from './FilterContact/FilterContact';
 import { Section } from './Section/Section';
 import { nanoid } from 'nanoid';
 import css from './App.module.css';
+import { useLocalStorage } from './hooks/useLocalStorage';
 
 
-export const App =() => {
-  const [contacts, setContacts] = useState([]);
+export const App = () => {
+  const [contacts, setContacts] = useLocalStorage('');
+  // const [contacts, setContacts] = useState(()=> {
+  //   return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+  // });
+
   const [filter, setFilter] = useState('');
 
+  // useEffect(() => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
+
   const addNewContact = ({ name, number }) => {
-        const newContact = {
-          name,
-          number,
-          id: nanoid(),
-        };
-      
-   
-        const isContactExist = contacts.find(
-          contact => contact.name.toLowerCase() === name.toLowerCase()
-        );
-        isContactExist
-          ? alert(`${name} is alreadi in contacts`)
-          : setContacts( contacts  => [newContact, ...contacts]);
-        }
-              
-    
-      const hendleDeleteContact = (id) => {
-        
-        setContacts(contacts  => (
-          contacts = contacts.filter(contact => contact.id !== id)
-        ));
-      };
-    
-     const  hendleFilter = (event) => {
-        setFilter( event.target.value );
-      };
-    
-      const getVisibleContacts = () => {
-        // const { filter, contacts } = this.state;
-        const normalizedFilter = filter.toLowerCase();
-        return contacts.filter(contact =>
-          contact.name.toLowerCase().includes(normalizedFilter)
-        );
-      };
+    const newContact = {
+      name,
+      number,
+      id: nanoid(),
+    };
+
+    const isContactExist = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    isContactExist
+      ? alert(`${name} is alreadi in contacts`)
+      : setContacts(contacts => [newContact, ...contacts]);
+  };
+
+  const hendleDeleteContact = id => {
+    setContacts(
+      contacts => (contacts = contacts.filter(contact => contact.id !== id))
+    );
+  };
+
+  const hendleFilter = event => {
+    setFilter(event.target.value);
+  };
+
+  const getVisibleContacts = () => {
+    // const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
 
   return (
     <div className={css.container}>
-        <Section title="Phonebook">
-          <FormContact onSubmit={addNewContact} />
-        </Section>
-        <Section title="Contacts">
-          <FilterContact
-            value={filter}
-            hendleFilter={hendleFilter}/>
+      <Section title="Phonebook">
+        <FormContact onSubmit={addNewContact} />
+      </Section>
+      <Section title="Contacts">
+        <FilterContact value={filter} hendleFilter={hendleFilter} />
 
-          <ContactList
-            contacts={getVisibleContacts()}
-            hendleDeleteContact={hendleDeleteContact}
-          />
-        </Section>
-      </div>
-
-  )
-}
+        <ContactList
+          contacts={getVisibleContacts()}
+          hendleDeleteContact={hendleDeleteContact}
+        />
+      </Section>
+    </div>
+  );
+};
 
 // export class App extends Component {
 //   state = {
@@ -79,9 +82,9 @@ export const App =() => {
 //     filter: '',
 //   };
 
-  // formSubmitHandler = data => {
-  //   console.log(data);
-  // };
+// formSubmitHandler = data => {
+//   console.log(data);
+// };
 // componentDidMount(){
 //     const contactsFromLocal= localStorage.getItem('contacts');
 //     const parsetContacts = JSON.parse(contactsFromLocal);
@@ -89,7 +92,7 @@ export const App =() => {
 //     if(parsetContacts){
 //        this.setState ({contacts: parsetContacts});
 //     }
-  
+
 //   }
 
 //   componentDidUpdate(_, prevState){
@@ -116,7 +119,7 @@ export const App =() => {
 //   };
 
 //   hendleDeleteContact = id => {
-    
+
 //     this.setState(( {contacts})  => ({
 //       contacts: contacts.filter(contact => contact.id !== id),
 //     }));
@@ -135,7 +138,7 @@ export const App =() => {
 //   };
 
 //   render() {
-    // const visibleContacts = this.getVivsibleContacts();
+// const visibleContacts = this.getVivsibleContacts();
 //     return (
 //       <div className={css.container}>
 //         <Section title="Phonebook">
